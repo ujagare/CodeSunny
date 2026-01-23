7f6035720ed09a44a0ba768edffc819719fa7cab
CodeSunny Developer <developer@codesunny.com>
2026-01-23 23:30:48 +0530

    Fix: Remove duplicate BrowserRouter

--- src/Utils/Routes.jsx
+++ src/Utils/Routes.jsx
@@ -9,24 +9,22 @@ import DigitalMarketing from "../Components/DigitalMarketing";
 import EcommerceSolutions from "../Components/EcommerceSolutions";
 import SEOOptimization from "../Components/SEOOptimization";
 import CloudSolutions from "../Components/CloudSolutions";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/web-development" element={<WebDevelopment />} />
-        <Route path="/services/ui-ux-design" element={<UiUxDesign />} />
-        <Route path="/services/digital-marketing" element={<DigitalMarketing />} />
-        <Route path="/services/ecommerce-solutions" element={<EcommerceSolutions />} />
-        <Route path="/services/seo-optimization" element={<SEOOptimization />} />
-        <Route path="/services/cloud-solutions" element={<CloudSolutions />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/services/web-development" element={<WebDevelopment />} />
+      <Route path="/services/ui-ux-design" element={<UiUxDesign />} />
+      <Route path="/services/digital-marketing" element={<DigitalMarketing />} />
+      <Route path="/services/ecommerce-solutions" element={<EcommerceSolutions />} />
+      <Route path="/services/seo-optimization" element={<SEOOptimization />} />
+      <Route path="/services/cloud-solutions" element={<CloudSolutions />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 };
 
